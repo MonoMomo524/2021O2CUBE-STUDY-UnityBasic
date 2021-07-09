fc0d1d4c435d0d7ae72e2ff74f5ca9dd1ac9df11
Haebny <37385962+Haebny@users.noreply.github.com>
2021-07-09 21:45:39 +0900

    Example Commit
    하하호호

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 하하 호호 바보 멍충이 하하 호호
+        // 취업 파이팅 취업 추카푸카
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
